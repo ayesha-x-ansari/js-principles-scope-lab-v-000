e7ed052dafddbcbf636d65d8b7557a1b089f44c3
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-15 14:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your solution in this file!
 
-const leastFavoriteCustomer  = 'Bob';
+//const leastFavoriteCustomer  = 'Bob';
 var customerName  = 'bob';
+/*
 function upperCaseCustomerName() {
   return customerName.toUpperCase();
 }
