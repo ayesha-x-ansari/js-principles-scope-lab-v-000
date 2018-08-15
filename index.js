a4a98e1495fdc46c70840ad25e8566be7458f1ca
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-15 14:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+const leastFavoriteCustomer  = 'Bob'; 
+var customerName  = 'bob';
+function upperCaseCustomerName() {
+  return upcase.customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  
+}
+
+function changeleastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Nancy';
+}
+
+
